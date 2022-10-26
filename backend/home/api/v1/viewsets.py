c24710097695f2f6a2c0f9f250b44bcbed4b8e38
Crowdbotics <engineering@crowdbotics.com>
2022-10-26 21:29:44 +0000

    Update models

--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -1,3 +1,7 @@
+from rest_framework import viewsets
+from home.models import Profile
+from .serializers import ProfileSerializer
+from rest_framework import authentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.authtoken.models import Token
@@ -28,3 +32,8 @@ class LoginViewSet(ViewSet):
         token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
         return Response({"token": token.key, "user": user_serializer.data})
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = ProfileSerializer
+    authentication_classes = (authentication.SessionAuthentication, authentication.TokenAuthentication)
+    queryset = Profile.objects.all()
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+class Profile(models.Model):
+    'Generated Model'
+    user = models.ForeignKey("users.User",on_delete=models.CASCADE,related_name="profile_user",)
+    phone = models.CharField(max_length=20,)
+    is_phone_authenticated = models.BooleanField()
